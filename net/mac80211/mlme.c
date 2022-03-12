a2b08af2ca20cb1cb352928685055728f7fb67d9
Johannes Berg <johannes.berg@intel.com>
2022-03-12 22:19:58 +0100

    mac80211: always have ieee80211_sta_restart()
    commit 3fa5a0f5b0d69e31c6476cd81afeca3cc25a4927 upstream.
    
    When CONFIG_PM isn't defined we don't have the function
    ieee80211_sta_restart() compiled in, but we always need
    it now for firmware restart. Move it out of the ifdef.
    
    Fixes: 7d352ccf1e99 ("mac80211: Add support to trigger sta disconnect on hardware restart")
    Link: https://lore.kernel.org/r/20220312221957.1fa96c72db51.I8ecaa5f9402fede0272161e0531ab930b97fba3e@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -4810,6 +4810,7 @@ void ieee80211_mgd_quiesce(struct ieee80211_sub_if_data *sdata)
 
 	sdata_unlock(sdata);
 }
+#endif
 
 void ieee80211_sta_restart(struct ieee80211_sub_if_data *sdata)
 {
@@ -4845,7 +4846,6 @@ void ieee80211_sta_restart(struct ieee80211_sub_if_data *sdata)
 
 	sdata_unlock(sdata);
 }
-#endif
 
 /* interface setup */
 void ieee80211_sta_setup_sdata(struct ieee80211_sub_if_data *sdata)
