206e4aeaa2b0aaf53eb11a129cc75b0340489b05
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-11-17 21:13:41 +0100

    build_kernel.sh: Deleted old useless script

--- build_kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PATH=$(pwd)/toolchain/clang-r498229b/bin:$PATH
-export PATH=$(pwd)/toolchain/path/linux-x86:$(pwd)/toolchain/gas/linux-x86:$PATH
-
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 O=/home/romy/romy/a53/out CROSS_COMPILE=$(pwd)/toolchain/clang-r498229b/bin/aarch64-linux-gnu- s5e8825-a53xxx_defconfig
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 O=/home/romy/romy/a53/out CROSS_COMPILE=$(pwd)/toolchain/clang-r498229b/bin/aarch64-linux-gnu- -j$(nproc --all)
