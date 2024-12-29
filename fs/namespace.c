586ce25ee11b45867fdf976c2a4a71d476b3dddd
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-12-29 19:30:39 -0300

    treewide: patch for susfs
    * Rev: 946ad8fec4e6191d8a44d289a2cdf235bc44a682
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3974,7 +3974,7 @@ SYSCALL_DEFINE5(mount, char __user *, dev_name, char __user *, dir_name,
 		goto out_data;
 
 	ret = do_mount(kernel_dev, dir_name, kernel_type, flags, options);
-#ifdef CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT
+#if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT) && defined(CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT)
 	// Just for the compatibility of Magic Mount KernelSU
 	if (!ret && susfs_is_current_ksu_domain()) {
 		susfs_auto_add_sus_ksu_default_mount(dir_name);
